9ca59fb9bb1a65acf251fda9f0a81e3c7ac2c9da
digital-animal <zahidulislamssf@gmail.com>
2022-12-05 16:27:46 +0600

    final push

--- src/main/java/com/zahid/todo/controllers/TodoController.java
+++ src/main/java/com/zahid/todo/controllers/TodoController.java
@@ -7,53 +7,36 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.zahid.todo.models.Todo;
-import com.zahid.todo.repositories.TodoRepository;
 import com.zahid.todo.services.TodoService;
 
 @Controller
 public class TodoController {
     private final Logger logger = LoggerFactory.getLogger(TodoController.class);
 
-    // @Autowired
-    // private TodoItemRepository todoItemRepository; // bad idea
-
     @Autowired
     private TodoService todoService; // better idea
 
-    @GetMapping("/todo/all") 
+    @GetMapping("/todo/all")
     public ModelAndView getAllTodo() {
         logger.debug("request to GET index");
         ModelAndView modelAndView = new ModelAndView("index");
 
         modelAndView.addObject("todoItems", todoService.getAllTodoItems());
-        
+
         return modelAndView;
     }
-    
+
     @GetMapping("/todo/{id}")
     public ModelAndView getOneTodo(@PathVariable("id") Long id) {
         ModelAndView modelAndView = new ModelAndView("edit-form");
-        // logger.info("Todo Id = {}", id);
-        // logger.info("New Description = {}", (String)model.getAttribute("description"));
-        // logger.info("New Description = {}", todoItem.getDescription());
-       
-        // Todo t = todoService.getTodoItem(id);
-        // t.setDescription((String)model.getAttribute("description"));
-        // t.setDescription(todoItem.getDescription());
-        // t.setCompleted((Boolean)model.getAttribute("completed"));
-        // t.setCompleted(todoItem.getCompleted());
-        // t.setLastModifiedDate(Instant.now());
 
-        // todoService.updateTodoItem(t);
         modelAndView.addObject("todo", todoService.getTodo(id));
 
         return modelAndView;
@@ -61,15 +44,12 @@ public class TodoController {
 
     @PostMapping("/todo/{id}")
     public String updateTodo(@ModelAttribute Todo todoItem, @PathVariable("id") Long id, Model model) {
-        // logger.info("Todo Id = {}", id);
-        // logger.info("New Description = {}", (String)model.getAttribute("description"));
+
         logger.info("New Description = {}", todoItem.getDescription());
-       
+
         Todo t = todoService.getTodo(id);
-        // Todo t = todoItem;
-        // t.setDescription((String)model.getAttribute("description"));
+
         t.setDescription(todoItem.getDescription());
-        // t.setCompleted((Boolean)model.getAttribute("completed"));
         t.setCompleted(todoItem.getCompleted());
         t.setLastModifiedDate(Instant.now());
 
@@ -91,13 +71,12 @@ public class TodoController {
     public String createTodo(@ModelAttribute Todo todoItem, Model model) {
         logger.info("Todo = {}", todoItem);
 
-        Todo t = new Todo(todoItem.getDescription());       
+        Todo t = new Todo(todoItem.getDescription());
         todoService.addTodo(t);
 
         return "create-done";
     }
 
-    
     @GetMapping("/todo/delete/{id}")
     public ModelAndView deletePrompt(@PathVariable("id") Long id) {
         logger.info("Todo Id = {}", id);
@@ -110,7 +89,7 @@ public class TodoController {
     @PostMapping("/todo/delete/{id}")
     public String deleteTodo(@PathVariable("id") Long id) {
         logger.info("Todo Id = {}", id);
-   
+
         todoService.deleteTodo(id);
 
         return "redirect:/todo/all";
